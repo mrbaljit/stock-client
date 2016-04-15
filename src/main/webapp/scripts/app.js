3e0dc56b2910b5f8c70d6ce175e68c9b887e60ec
Baljit <baljit.singh@suncorp.co.nz>
2016-04-15 18:12:58 +1200

    renamed controllers

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,53 +1,25 @@
 (function (global, angular) {
     'use strict';
 
-});
+    angular.module("nz.com.stock", [
+        'nz.com.stock.liststock',
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.router',
+    ]).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
+        $urlRouterProvider.otherwise('/');
 
-angular.module('todoapp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    "ui.router",
-]).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-
-    $urlRouterProvider.otherwise('/');
-
-    $stateProvider.state('list', {
+        $stateProvider.state('list', {
             url: '/',
-        views: {
-            'content': {
-                templateUrl: 'views/list.html',
-                controller: 'ListCtrl'
+            views: {
+                'content': {
+                    templateUrl: 'views/list.html',
+                    controller: 'ListController'
+                }
             }
-        }
-    })
+        })
 
-}]).controller('ListCtrl', function ($scope, $http, $filter) {
-
-    var orderBy = $filter('orderBy');
-
-   /* $http.get('http://localhost:7090/todo/getSample').success(function (data) {
-        $scope.todos = data;
-
-        $scope.order = function (predicate, reverse) {
-            $scope.todos = orderBy($scope.todos, predicate, reverse);
-        };
-
-    }).error(function (data, status) {
-        console.log('Error ' + data)
-    })
-*/
-    $http.get('http://localhost:9090/stock/allStock').success(function (data) {
-        $scope.todos = data;
-        console.log(data)
-
-       /* $scope.order = function (predicate, reverse) {
-            $scope.todos = orderBy($scope.todos, predicate, reverse);
-        };*/
-
-    }).error(function (data, status) {
-        console.log('Error ' + data)
-    })
-
-});
+    }])
+})(this, angular);
