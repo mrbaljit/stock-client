3a204c29755c090684b2a1400ddc8ae2ced3c56c
Baljit <baljit.singh@suncorp.co.nz>
2016-04-29 13:58:33 +1200

    changed the layout

---
+++ src/main/webapp/scripts/productService.js
@@ -0,0 +1,42 @@
+(function (global, angular) {
+    'use strict';
+
+    angular.module("nz.com.product.productService", [])
+        .service("productService", ['$http', '$filter', '$state', '$q',
+            function ($http, $filter, $state, $q) {
+
+                return{
+                    getAllProducts:getAllProducts,
+                    createUpdateProduct : createUpdateProduct
+                }
+
+
+        function createUpdateProduct(dataObj) {
+
+            var res = $http.post('http://localhost:9090/product/createUpdateProduct', dataObj);
+
+            res.success(function(data, status, headers, config) {
+                console.log("eeee");
+                $state.go('listProducts');
+            });
+            res.error(function(data, status, headers, config) {
+                alert( "failure message: " + JSON.stringify({data: data}));
+            });
+        }
+
+
+       function getAllProducts() {
+           var def = $q.defer();
+            $http.get('http://localhost:9090/product/allProducts').success(function (data) {
+                console.log(data, " vvvv");
+                def.resolve(data);
+                //return data;
+            }).error(function (data, status) {
+                console.log('Error ' + data)
+            });
+           return def.promise;
+        }
+
+    }]);
+    
+})(this, angular);
